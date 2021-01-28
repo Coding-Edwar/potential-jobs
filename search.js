fcdfb8d2ca54385b2b3636547d62cf188923c812
edeced <ed_ced@icloud.com>
2021-01-28 11:37:30 -0600

    change api to https, styles results list

--- search.js
+++ search.js
@@ -22,20 +22,20 @@ searchForm.addEventListener('submit', async function (e) {
   let jobsFound = jobResults.count
 
   resultSection.innerHTML = `
-<div id="result-section" class="p-16">
-<h1>${jobsFound} jobs found for <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
+<div id="result-section" class="p-4 divide-solid divide-y-2 divide-black">
+<div>You searched for: <strong>${jobName}</strong> in <strong>${SEARCH_COUNTRY.toUpperCase()}</strong> found:${jobsFound}</div>
 </div>`
   jobs.forEach(function (job) {
     const div = document.createElement('div')
-    div.innerHTML = `<h4><a href="${job.redirect_url}">${job.title}</a> | ${job.location.display_name}</h4> <p>${job.description}</p>`
+    div.innerHTML = `<div class="p-4 divide-solid divide-y-2 divide-black"><p>${job.title}</p> | ${job.location.display_name}</div> <p class="p-4 divide-solid divide-y-2 divide-black">${job.description}</p>`
     resultSection.appendChild(div)
   })
 })
-
+// href="${job.redirect_url}"
 async function searchJobs (jobString, jobsCount = 3, country = SEARCH_COUNTRY) {
 
   // see https://gist.github.com/imdeletosh/f13c339b5f8e0a62ebe973a4ac86c3c0 for a breakdown of this
-  const url = `http://api.adzuna.com/v1/api/jobs/${country}/search/1?app_id=a7547f34&app_key=92b62ee9bfd90c11c097004b51438beb&results_per_page=${jobsCount}&what=${jobString}&content-type=application/json`
+  const url = `https://api.adzuna.com/v1/api/jobs/${country}/search/1?app_id=a7547f34&app_key=92b62ee9bfd90c11c097004b51438beb&results_per_page=${jobsCount}&what=${jobString}&content-type=application/json`
   console.log(url)
 
 //   https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=a7547f34&app_key=92b62ee9bfd90c11c097004b51438beb&results_per_page=20&what=javascript%20developer&content-type=application/json
